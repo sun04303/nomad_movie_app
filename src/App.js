3eea915e86559e0a42ecd382211bcc1709b787e2
sun04303 <sin243996@gmail.com>
2021-01-12 02:06:57 +0900

    2021-01-12

--- src/App.js
+++ src/App.js
@@ -1,55 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const movieILike = [
-  {
-    id: 1,
-    name: "Star Wars",
-    image: "https://awesomewallpaper.files.wordpress.com/2011/01/star_wars.jpg",
-    rating: 4.7
-  },
-  {
-    id: 2,
-    name: "The Lord of the Rings",
-    image: "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781608873821/the-lord-of-the-rings-9781608873821_hr.jpg",
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: "Harry Potter",
-    image: "https://static.booktopia.com.au/internals/9781608871131-1.jpg",
-    rating: 4.8
-  },
-  {
-    id: 4,
-    name: "Hobbit",
-    image: "https://jessmountifield.co.uk/wp-content/uploads/2015/01/Hobbit-Dwarves.jpg",
-    rating: 4.7
+class App extends React.Component {
+  state = {
+    isLoading: true
+  };
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
   }
-];
 
-function Movie({ name, image, rating }) {
-  return <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={image} alt={name}></img>
-    </div>
-}
+  render() {
+    const { isLoading } = this.state;
 
-Movie.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div>
-      {movieILike.map(movie => (
-        <Movie key={movie.id} name={movie.name} image={movie.image} rating={movie.rating} />
-      ))}
-    </div>
-  );
+    return(
+      <div>
+        {isLoading ? "Loading" : "We are ready"}
+      </div>
+    );
+  }
 }
 
 export default App;
